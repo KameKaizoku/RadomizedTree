7cc0513469acc1618b56cf714683f8a075b09beb
KameKaizoku <annafomina01@gmail.com>
2020-05-04 18:16:07 +0300

    program

---
+++ RadomizedTree/Program.cs
@@ -0,0 +1,583 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RadomizedTree
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      MyTree A = new MyTree();
+      MyTree B = new MyTree();
+      Random rnd = new Random();
+      var array = Enumerable.Range(1,20).OrderBy(i => rnd.Next()).ToArray();
+      var array2 = Enumerable.Range(5,10).OrderBy(i => rnd.Next()).ToArray();
+
+      foreach (var VARIABLE in array)
+      {
+        A.AddRandom(VARIABLE);
+      }
+      foreach (var VARIABLE in array2)
+      {
+        B.AddRandom(VARIABLE);
+      } 
+      Console.WriteLine("Tree A\n");
+      TreePrinter.PrintMyTree(A);
+      Console.WriteLine("Tree B\n");
+      TreePrinter.PrintMyTree(B);
+      Console.WriteLine();
+      var t =A.Right(A.Root);
+      A.Delete(A.Root, B);
+      Console.WriteLine("\nTree A\n");
+      TreePrinter.PrintMyTree(A);
+    //  Console.WriteLine(B.Search(19));
+    }
+
+    public class MyTree
+    {
+      public int Size { get; set; }
+      public MyNode Root { get; set; }
+
+      
+      public MyNode Right(MyNode node)
+      {
+        if (node == null) return null;
+        if (node == Root)
+        {
+          Console.Write($"{node.Key} ");
+          Right(node.L_Son);
+        }
+        else
+        {
+          if (node.L_Son != null)
+          {
+            Console.Write($"{node.Key} ");
+            Right(node.L_Son);
+          }
+          else
+          {
+            if (node.R_Bro != null)
+            {
+              Console.Write($"{node.Key} ");
+              while (node.R_Bro == null)
+              {
+                node = GetParet(node);
+              }
+
+              Right(node.R_Bro);
+            }
+
+            if (node.L_Son == null && node.R_Bro == null)
+            {
+              Console.Write($"{node.Key} ");
+              while(node != null && node.R_Bro==null)
+                node=GetParet(node);
+              if (node == Root) return null;
+              if (node != null) Right(node.R_Bro);
+            }
+          }
+        }
+        return null;
+      }
+
+    public void Delete(MyNode A, MyTree B)
+      {
+        if(A==null) return;
+    //  Console.WriteLine();
+  //  Console.WriteLine(A.Key);
+ //  TreePrinter.PrintMyTree(this);
+    
+        bool found = B.Search(A.Key);
+
+        if (!found)
+        {
+
+          if (A.L_Son == null)
+          {
+            MyNode parent = GetParet(A);
+            if (parent != null) parent.L_Son = A?.R_Bro;
+            Delete(parent.L_Son, B);
+            return;
+            
+          }
+
+          if (A.L_Son != null && A.L_Son.R_Bro == null)
+          {
+            MyNode parent = GetParet(A);
+            if (parent != null)
+            {
+              
+               // parent.L_Son.Key = (int) A?.L_Son.Key;
+               if (A.L_Son != null)
+               {
+                 //parent.L_Son.Key = A.L_Son.Key;
+                 if (parent.L_Son.Key == A.Key)
+                 {
+                   parent.L_Son = A?.L_Son;
+                   parent.L_Son.R_Bro = A?.R_Bro;
+                   Delete(parent.L_Son, B);
+                   return;
+                 }
+                 else
+                 {
+                   parent.L_Son.R_Bro = A?.L_Son;
+                   Delete(parent.L_Son.R_Bro, B);
+                 }
+                 //if(parent.L_Son.L_Son!=null)  parent.L_Son.L_Son = null;
+               }
+               else
+                 parent.L_Son = A?.R_Bro;
+               //A = A?.L_Son;
+                
+             // Delete(parent, B);
+              return;
+            }
+            else
+            {
+              Root.Key = A.L_Son.Key;
+              Root.L_Son = A?.L_Son.L_Son;
+              Delete(Root, B);
+              return;
+            }
+          }
+
+          if (A.L_Son != null && A.L_Son.R_Bro != null)
+          {
+            MyNode parent = GetParet(A);
+            MyNode temp = A.L_Son.R_Bro;
+
+            while (temp.L_Son != null && temp.L_Son.Key<temp.Key)
+            {
+              temp = temp.L_Son;
+            }
+            MyNode parent2 = GetParet(temp);
+            parent2 = GetParet(temp);
+          
+            if (parent2.Key != A.Key) parent2.L_Son = temp?.R_Bro;
+            else parent2.L_Son.R_Bro = null;
+
+            if (parent != null)
+            {
+               A.Key = temp.Key;
+              Delete(A, B);
+              return;
+            }
+            else
+            {
+              Root.Key = temp.Key;
+              Root.L_Son.R_Bro = A.L_Son?.R_Bro;
+              Delete(Root, B);
+              return;
+            }
+
+          }
+        }
+
+        if (A.L_Son != null)
+        {
+          Delete(A.L_Son, B);
+         // return;
+        }
+
+        if (A.R_Bro != null)
+        {
+          Delete(A.R_Bro, B);
+         // return;
+        }
+          
+        if ( A.R_Bro == null)
+        {
+          while(A != null && A.R_Bro==null)
+            A=GetParet(A);
+          if (A == Root) return ;
+          if(A==null) return;
+          if (A != null)
+          {
+            Delete(A.R_Bro, B);
+            return;
+          }
+           
+         // return;
+        }
+         // Delete(Root,);
+        return;
+        
+      }
+      
+    /* public void Delete(MyNode A,  MyTree B)
+      {
+        if(A==null) return;
+    //   TreePrinter.PrintMyTree(this);
+        var toDel = (B != null && B.Search(A.Key));
+      
+        if (!toDel)
+        {
+          
+          if (A != null && A?.L_Son == null)
+          {
+            MyNode parent = GetParet(A);
+            if (parent != null)
+            {
+              if (parent.L_Son.Key == A.Key) ;
+            }
+
+
+            else Root.L_Son = null;
+            Delete(parent,B);
+            return;
+          }
+
+          if (A != null && (A.L_Son != null && A.L_Son.R_Bro == null))
+          {
+            
+            MyNode parent = GetParet(A);
+           // MyNode lson = A?.L_Son;
+           // lson.R_Bro = A.R_Bro;
+            if (parent != null)
+            {
+              parent.L_Son = A?.L_Son;
+             Delete(parent.L_Son, B);
+              return;
+            }
+            else
+            {
+              Root = A.L_Son;
+              Root.L_Son = A.L_Son.L_Son;
+              Delete(Root, B);
+            }
+           
+            return;
+          }
+
+          if (A != null && A?.L_Son?.R_Bro != null)
+          {
+            
+            MyNode parent = GetParet(A);
+            
+              MyNode temp = A.L_Son.R_Bro;
+              while (temp.L_Son != null)
+              {
+                temp = temp.L_Son;
+              }
+            
+              MyNode parent2 = GetParet(temp);
+              if (parent2 != null)
+              {
+                if(parent2.L_Son.Key==temp.Key)
+                  parent2.L_Son = temp?.R_Bro;
+                else
+                {
+                  parent2.L_Son.R_Bro = null;
+                }
+              }
+
+             A.Key = temp.Key;
+         
+              Delete(A, B);
+              return;
+          }
+          
+        }
+
+        if (A.L_Son != null)
+        {
+          Delete(A.L_Son, B);
+          return;
+        }
+        else
+        {
+          if (A.R_Bro != null)
+          {
+            Delete(A.R_Bro, B);
+            return;
+          }
+          else
+          {
+            if ( A.R_Bro == null)
+            {
+              while(A != null && A.R_Bro==null)
+                A=GetParet(A);
+              if (A == Root) return ;
+              if (A != null)
+              {
+                Delete(A.R_Bro, B);
+                return;
+              }
+           
+              return;
+            }
+          }
+        }
+          
+        
+      }*/
+      
+      
+      public bool Search(int toFind)
+      {
+        bool inTree = false;
+        if (toFind == Root.Key) return true;
+        recurtion(Root);
+        
+        void recurtion(MyNode node)
+        {
+          if(node==null) return;
+          
+          if (node.Key == toFind)
+          {
+            inTree = true;
+            return;
+          }
+          if (toFind < node.Key)
+          {
+
+            if (node.L_Son == null)
+              return;
+           
+            if (node.L_Son.Key < toFind)
+            {
+              if(node.L_Son.L_Son.R_Bro==null) recurtion(node.L_Son);
+              recurtion(node.L_Son.L_Son.R_Bro);
+              return;
+            }
+            
+            recurtion(node.L_Son);
+          
+          }
+          else
+          {
+           
+            if (node.L_Son == null) return;
+            
+
+            if (node.L_Son.Key > toFind)
+            {
+              recurtion(node.L_Son);
+              return;
+            }
+
+            if (node.L_Son.R_Bro == null && node.L_Son!=null)
+            {
+              recurtion(node.L_Son);
+            }
+            recurtion(node.L_Son.R_Bro);
+          }
+        }
+         
+        return inTree;
+      }
+
+      public void InsertRoot(MyNode myNode)
+      {
+
+        while (myNode.Key != Root.Key)
+        {
+          MyNode parent = GetParet(myNode);
+
+          if (parent.Key > myNode.Key)
+            RotateR(myNode, parent);
+          else
+            RotateL(myNode, parent);
+
+          if (parent.Key == Root.Key)
+          {
+            Root = myNode;
+            break;
+          }
+          
+        }
+
+      }
+
+      void RotateR(MyNode myNode, MyNode parent)
+      {
+        MyNode A = myNode.L_Son;
+        MyNode B = A?.R_Bro;
+        MyNode C = myNode.R_Bro;
+
+        if (A != null && A.Key > myNode.Key)
+        {
+          B = A;
+          A = null;
+        }
+
+        if (A != null) A.R_Bro = parent;
+        if (B != null)
+        {
+          parent.L_Son = B;
+          B.R_Bro = C;
+        }
+        else
+        {
+          parent.L_Son = C;
+        }
+
+        if (A == null)
+        {
+          myNode.L_Son = parent;
+        }
+        else
+        {
+          A.R_Bro = parent;
+        }
+
+        MyNode grandpa = GetParet(parent);
+        if (grandpa == null) return;
+        if (grandpa.L_Son.Key == parent.Key) grandpa.L_Son = myNode;
+        else  grandpa.L_Son.R_Bro = myNode;
+       
+      }
+
+      void RotateL(MyNode myNode, MyNode parent)
+      {
+        MyNode A = parent.L_Son;
+        MyNode B = myNode.L_Son;
+        MyNode C = B?.R_Bro;
+
+        if (parent.L_Son.Key==myNode.Key)
+        {
+          A = null;
+        }
+
+        if (B != null && B.Key>myNode.Key)
+        {
+          C = B;
+          B = null;
+        }
+
+        if (A != null)
+        {
+          A.R_Bro = B;
+        }
+        
+        parent.L_Son = A;
+        parent.R_Bro = C;
+        myNode.L_Son = parent;
+        
+        MyNode grandpa = GetParet(parent);
+        if (grandpa == null) return;
+        if (grandpa.L_Son.Key == parent.Key) grandpa.L_Son = myNode;
+        else  grandpa.L_Son.R_Bro = myNode;
+
+      }
+      
+      public MyNode GetParet(MyNode me)
+      {
+        MyNode checking = Root;
+
+        if (me == Root) return null;
+        
+        CheckParent();
+        return checking;
+        
+        void CheckParent()
+        {
+          if(checking==null) return;
+          
+          MyNode bro=null;
+          MyNode child = checking.L_Son;
+          if (child != null)
+          {
+            bro = child.R_Bro;
+          }
+
+          if ((child!=null && child.Key == me.Key) || (bro!=null && bro.Key == me.Key)) return;
+          if (checking.Key < me.Key && bro != null)
+            checking = bro;
+          else
+            checking = child;
+          CheckParent();
+        }
+        
+      }
+
+      public void AddRandom(int key)
+      {
+       
+        MyNode adding = new MyNode(key);
+        Add(adding);
+        var random = new Random().Next(1, Size+1);
+        if(random==Size)
+          InsertRoot(adding);
+      }
+      public void Add(MyNode k)
+      {
+        Size++;
+        
+        if (Root == null)
+        {
+          Root = new MyNode(k.Key);
+          return;
+        }
+        
+        recurtion(Root);
+        
+        void recurtion(MyNode node)
+        {
+          if (k.Key < node.Key)
+          {
+            if (node.L_Son == null)
+            {
+              node.L_Son = k;
+              
+             
+              return;
+            }
+
+            if (node.L_Son.Key < node.Key)
+            {
+              recurtion(node.L_Son);
+              return;
+            }
+
+            MyNode bro = node.L_Son;
+            node.L_Son = k;
+            node.L_Son.R_Bro = bro;
+            
+          }
+          else
+          {
+            if (node.L_Son == null)
+            {
+              node.L_Son = k;
+            
+              return;
+            }
+
+            if (node.L_Son.Key > node.Key)
+            {
+              recurtion(node.L_Son);
+              return;
+            }
+
+            if (node.L_Son.R_Bro == null)
+            {
+              node.L_Son.R_Bro = k;
+              
+              return;
+            }
+            recurtion(node.L_Son.R_Bro);
+          }
+        }
+
+        
+      }
+    }
+
+    public class MyNode 
+    {
+      public int Key { get; set; }
+      public MyNode L_Son { get; set; }
+      public MyNode R_Bro { get; set; }
+      
+      
+      public MyNode(int key)
+      {
+        Key = key;
+      }
+    }
+  }
+}
---
+++ RadomizedTree/TreePrinter.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+//using static RadomizedTree.Program;
+
+namespace RadomizedTree
+{
+
+    public class BNode
+    {
+        public int Item;
+        public BNode Right;
+        public BNode Left;
+
+        public BNode(int item)
+        {
+            this.Item = item;
+        }
+    }
+
+    internal class TreePrinter
+    {
+        public static void PrintMyTree(Program.MyTree myTree, int topMargin = 0, int leftMargin = 2)
+        {
+            BNode firstNode = new BNode(0);
+            Add(firstNode, myTree.Root);
+            Print(firstNode, topMargin, leftMargin);
+            void Add(BNode bNode, Program.MyNode myNode)
+            {
+                if (myNode != null)
+                {
+                    bNode.Item = myNode.Key;
+                    if (myNode.L_Son != null)
+                    {
+                        Program.MyNode child = myNode.L_Son;
+
+                        if (child.Key < bNode.Item)
+                        {
+                            bNode.Left=new BNode(0);
+                            Add(bNode.Left, child);
+                            Program.MyNode bro = child.R_Bro;
+                            if (bro != null)
+                            {
+                                bNode.Right=new BNode(0);
+                                Add(bNode.Right, bro);
+                            }
+                            
+                        }
+                        else
+                        {
+                            bNode.Right=new BNode(0);
+                            Add(bNode.Right, child);
+                        }
+                    }
+
+                }
+            }
+        
+
+        }
+
+        class NodeInfo
+        {
+            public BNode Node;
+            public string Text;
+            public int StartPos;
+
+            public int Size
+            {
+                get { return Text.Length; }
+            }
+
+            public int EndPos
+            {
+                get { return StartPos + Size; }
+                set { StartPos = value - Size; }
+            }
+
+            public NodeInfo Parent, Left, Right;
+        }
+
+        public static void Print(BNode root, int topMargin = 2, int leftMargin = 2)
+        {
+            if (root == null) return;
+            int rootTop = Console.CursorTop + topMargin;
+            var last = new List<NodeInfo>();
+            var next = root;
+            for (int level = 0; next != null; level++)
+            {
+                var item = new NodeInfo {Node = next, Text = next.Item.ToString(" 0 ")};
+                if (level < last.Count)
+                {
+                    item.StartPos = last[level].EndPos + 1;
+                    last[level] = item;
+                }
+                else
+                {
+                    item.StartPos = leftMargin;
+                    last.Add(item);
+                }
+
+                if (level > 0)
+                {
+                    item.Parent = last[level - 1];
+                    if (next == item.Parent.Node.Left)
+                    {
+                        item.Parent.Left = item;
+                        item.EndPos = Math.Max(item.EndPos, item.Parent.StartPos);
+                    }
+                    else
+                    {
+                        item.Parent.Right = item;
+                        item.StartPos = Math.Max(item.StartPos, item.Parent.EndPos);
+                    }
+                }
+
+                next = next.Left ?? next.Right;
+                for (; next == null; item = item.Parent)
+                {
+                    Print(item, rootTop + 2 * level);
+                    if (--level < 0) break;
+                    if (item == item.Parent.Left)
+                    {
+                        item.Parent.StartPos = item.EndPos;
+                        next = item.Parent.Node.Right;
+                    }
+                    else
+                    {
+                        if (item.Parent.Left == null)
+                            item.Parent.EndPos = item.StartPos;
+                        else
+                            item.Parent.StartPos += (item.StartPos - item.Parent.EndPos) / 2;
+                    }
+                }
+            }
+
+            Console.SetCursorPosition(0, rootTop + 2 * last.Count - 1);
+        }
+
+        private static void Print(NodeInfo item, int top)
+        {
+            SwapColors();
+            Print(item.Text, top, item.StartPos);
+            SwapColors();
+            if (item.Left != null)
+                PrintLink(top + 1, "┌", "┘", item.Left.StartPos + item.Left.Size / 2, item.StartPos);
+            if (item.Right != null)
+                PrintLink(top + 1, "└", "┐", item.EndPos - 1, item.Right.StartPos + item.Right.Size / 2);
+        }
+
+        private static void PrintLink(int top, string start, string end, int startPos, int endPos)
+        {
+            Print(start, top, startPos);
+            Print("─", top, startPos + 1, endPos);
+            Print(end, top, endPos);
+        }
+
+        private static void Print(string s, int top, int left, int right = -1)
+        {
+            Console.SetCursorPosition(left, top);
+            if (right < 0) right = left + s.Length;
+            while (Console.CursorLeft < right) Console.Write(s);
+        }
+
+        private static void SwapColors()
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = Console.BackgroundColor;
+            Console.BackgroundColor = color;
+        }
+    }
+}
